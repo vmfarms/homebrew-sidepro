5cc9fb9afaf6911725b546df85e78d8472534139
Hany Fahim <hany@vmfarms.com>
2024-09-10 10:36:16 -0400

    first release

---
+++ Formula/sidepro.rb
@@ -0,0 +1,46 @@
+class Sidepro < Formula
+    desc "The SidePro CLI"
+    homepage "https://sidepro.cloud"
+    url "https://shared-clients-api.minio.beta.stack.io/sidepro-cli/channels/beta/sidepro-darwin-x64.tar.gz"
+    sha256 "4e855b3612c27c6ad7e3dcc720d24135be9384d9a6a03f7dcbc8a3eb8e2f8aa6"
+    version "v0.4.1"
+    version_scheme 1
+    
+    depends_on "epinio"
+  
+    on_macos do
+      on_intel do
+        url "https://shared-clients-api.minio.beta.stack.io/sidepro-cli/channels/beta/sidepro-darwin-x64.tar.gz"
+        sha256 "4e855b3612c27c6ad7e3dcc720d24135be9384d9a6a03f7dcbc8a3eb8e2f8aa6"
+      end
+    end
+  
+    def install
+      inreplace "bin/sidepro", /^CLIENT_HOME=/, "export SIDEPRO_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+      libexec.install Dir["*"]
+      bin.install_symlink libexec/"bin/sidepro"
+  
+      # bash_completion.install libexec/"autocomplete-scripts/brew/bash" => "sidepro"
+      # zsh_completion.install libexec/"autocomplete-scripts/brew/zsh/_sidepro"
+    end
+  
+    def caveats; <<~EOS
+      To use the SidePro CLI's autocomplete --
+        Via homebrew's shell completion:
+          1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
+              NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
+                    and called, either explicitly or via a framework like oh-my-zsh.
+          2) Then run
+            $ sidepro autocomplete --refresh-cache
+        OR
+        Use our standalone setup:
+          1) Run and follow the install steps:
+            $ sidepro autocomplete
+    EOS
+    end
+  
+    test do
+      system bin/"sidepro", "--version"
+    end
+  end
+  
